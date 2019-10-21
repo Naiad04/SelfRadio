fd26b416bded79e6a941fd4de2d8d5f02383f808
nycrera <alperenyurdakul7@gmail.com>
2019-10-21 13:54:38 +0300

    Update uploadMedia.ts

--- src/middlewares/uploadMedia.ts
+++ src/middlewares/uploadMedia.ts
@@ -5,9 +5,7 @@
 * After this middleware is called req.file will be existent.
 */
 import { Request, Response, NextFunction } from "express";
-import * as jwt from "jsonwebtoken";
-import config from "../config";
-import multer from "multer"; //We don't install types for the multer since @types/multer package is not a es6 module.  (boo)
+import multer from "multer";
 
 export const uploadMedia = (req: Request, res: Response, next: NextFunction) => {
     const upload = multer({ storage: multer.diskStorage({}) });
