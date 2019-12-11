63ab6c82b3de69b5486257f72581f794933097bc
nycrera <alperenyurdakul7@gmail.com>
2019-12-11 04:41:32 +0200

    Update source.ts

--- src/global_classes/source.ts
+++ src/global_classes/source.ts
@@ -1,6 +1,10 @@
 import Config from "../config"
+import { spawn, ChildProcessWithoutNullStreams } from "child_process";
 export default class Source {
+    private process: ChildProcessWithoutNullStreams;
     constructor() {
-
+        this.process = spawn('ices', ['-h', Config.icecast.host, '-p', Config.icecast.port, '-P',
+            Config.icecast.password, '-m', Config.icecast.mountPoint, '-n', Config.icecast.streamName,
+            '-g', Config.icecast.genre, '-S', Config.icecast.moduleType]);
     }
 }
